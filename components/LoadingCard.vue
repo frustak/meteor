c3b8b4d539302b1ba55676091b94c6d815881e6a
frustak <soulburn007@gmail.com>
2023-09-13 00:35:43 +0330

    loading (1)

---
+++ components/LoadingCard.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="bg-neutral-200 w-20 h-28"></div>
+</template>
---
+++ pages/loading.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import _ from 'lodash';
+import { nanoid } from 'nanoid';
+
+const cards = _.range(8).map(() => ({ id: nanoid() }));
+</script>
+
+<template>
+    <main class="bg-indigo-400 min-h-screen">
+        <LoadingCard
+            v-for="(card, idx) in cards"
+            :key="card.id"
+            class="fixed left-1/2 top-1/2"
+            :style="{
+                transform: `translate(-50%, -50%) rotate(${
+                    idx * 45
+                }deg) translateY(175%)`,
+            }"
+        />
+    </main>
+</template>
+
+<!-- translate(${Math.sin(idx)  * 100}px, ${Math.cos(idx) * 100}px) -->
