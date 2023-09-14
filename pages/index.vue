1c37c880876bdfcece619c4c26a0caf0d06cddb0
frustak <soulburn007@gmail.com>
2023-09-14 20:41:11 +0330

    add home page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <main class="max-w-prose mx-auto pt-10 font-mono text-xs font-black">
+        <h1 class="text-9xl font-thin">👋😊</h1>
+        <div class="mt-12">
+            <h2>Here are some stuff I've implemented so far, Feel free to check them out.</h2>
+            <ul class="list-disc mt-6 text-lg">
+                <li>
+                    <NuxtLink to="/cards">The Cards</NuxtLink>
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
