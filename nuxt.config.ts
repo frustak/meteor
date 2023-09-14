ab89c9b4aa2434ac4903a5a6dab1b7a13bb725d5
frustak <soulburn007@gmail.com>
2023-09-14 20:40:18 +0330

    format nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        [
-            '@nuxtjs/google-fonts',
-            { 'Noto Sans': [100, 200, 300, 400, 500, 600, 700, 800, 900] },
-        ],
+        ['@nuxtjs/google-fonts', { 'Noto Sans': [100, 200, 300, 400, 500, 600, 700, 800, 900] }],
         '@nuxtjs/tailwindcss',
     ],
     devtools: { enabled: true },
-    build: {
-        transpile: ['gsap'],
-    },
-});
+    build: { transpile: ['gsap'] },
+})
