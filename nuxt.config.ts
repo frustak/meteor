48853d66d66abe5353debe03ce5b6c5e70586a7f
frustak <soulburn007@gmail.com>
2023-09-14 18:11:40 +0330

    transpile gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        ['@nuxtjs/eslint-module', { lintOnStart: false }],
         [
             '@nuxtjs/google-fonts',
             { 'Noto Sans': [100, 200, 300, 400, 500, 600, 700, 800, 900] },
@@ -9,4 +8,7 @@ export default defineNuxtConfig({
         '@nuxtjs/tailwindcss',
     ],
     devtools: { enabled: true },
+    build: {
+        transpile: ['gsap'],
+    },
 });
